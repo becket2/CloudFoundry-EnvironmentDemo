3ef8b7a63ce9bd930ce89bc51eae0d08a63cfc9e
becket2 <thean.beckerling@emc.com>
2015-09-10 11:19:22 -0400

    Update Application.java

--- src/main/java/com/gopivotal/cf/workshop/Application.java
+++ src/main/java/com/gopivotal/cf/workshop/Application.java
@@ -1,10 +1,10 @@
 package com.gopivotal.cf.workshop;
 
 import org.springframework.boot.SpringApplication;
-
 /**
  * Used to initialize the app when packaged as a JAR
  * @author kkrueger
+ * Hello
  */
 public class Application {
 
